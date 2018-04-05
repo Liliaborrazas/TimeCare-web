6bcb1860e44331a0ff4ed9207cdc78125f7571d3
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-04-05 20:50:34 +0200

    feat:first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/misc/login/login.component';
+import { routes } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
